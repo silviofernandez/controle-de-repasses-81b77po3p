473b0161e5f33b402835fd6a54433b214c3ede79
Skip <skip@usecurling.com>
2026-08-04 17:11:53 -0300

    feat: Add manual folder registration page

--- pocketbase/migrations/0006_add_owner_name.js
+++ pocketbase/migrations/0006_add_owner_name.js
@@ -3,15 +3,15 @@ migrate(
     const col = app.findCollectionByNameOrId('folders')
     if (!col.fields.getByName('owner_name')) {
       col.fields.add(new TextField({ name: 'owner_name' }))
+      app.save(col)
     }
-    app.save(col)
   },
   (app) => {
     const col = app.findCollectionByNameOrId('folders')
     const field = col.fields.getByName('owner_name')
     if (field) {
       col.fields.removeById(field.id)
+      app.save(col)
     }
-    app.save(col)
   },
 )
